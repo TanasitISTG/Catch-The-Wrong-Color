b83ff7cb936a5877586b71f22ceb15a79e0c9a37
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2023-02-06 12:47:18 +0700

    reworked the whole shiii

--- src/App.vue
+++ src/App.vue
@@ -1,274 +1,213 @@
 <script setup>
 import { ref } from 'vue';
+import colorJson from './assets/data/colors.json';
 
-const colorData = [
-    {
-        wrongColor: 'bg-red-700',
-        rightColor: 'bg-red-800'
-    },
-    {
-        wrongColor: 'bg-blue-600',
-        rightColor: 'bg-blue-700'
-    },
-    {
-        wrongColor: 'bg-green-400',
-        rightColor: 'bg-green-500'
-    },
-    {
-        wrongColor: 'bg-yellow-400',
-        rightColor: 'bg-yellow-500'
-    },
-    {
-        wrongColor: 'bg-purple-600',
-        rightColor: 'bg-purple-500'
-    },
-    {
-        wrongColor: 'bg-pink-900',
-        rightColor: 'bg-rose-900'
-    },
-    {
-        wrongColor: 'bg-indigo-800',
-        rightColor: 'bg-indigo-700'
-    },
-    {
-        wrongColor: 'bg-neutral-900',
-        rightColor: 'bg-neutral-800'
-    },
-    {
-        wrongColor: 'bg-emerald-200',
-        rightColor: 'bg-green-200'
-    },
-    {
-        wrongColor: 'bg-blue-700',
-        rightColor: 'bg-blue-800'
-    },
-    {
-        wrongColor: 'bg-teal-800',
-        rightColor: 'bg-teal-900'
-    },
-    {
-        wrongColor: 'bg-slate-600',
-        rightColor: 'bg-gray-600'
-    },
-    {
-        wrongColor: 'bg-amber-400',
-        rightColor: 'bg-yellow-400'
-    },
-    {
-        wrongColor: 'bg-indigo-700',
-        rightColor: 'bg-blue-800'
-    },
-    {
-        wrongColor: 'bg-teal-800',
-        rightColor: 'bg-teal-900'
-    },
-    {
-        wrongColor: 'bg-slate-600',
-        rightColor: 'bg-gray-600'
-    },
-    {
-        wrongColor: 'bg-amber-400',
-        rightColor: 'bg-yellow-400'
-    },
-    {
-        wrongColor: 'bg-emerald-200',
-        rightColor: 'bg-green-200'
-    },
-    {
-        wrongColor: 'bg-indigo-700',
-        rightColor: 'bg-blue-800'
-    },
-    {
-        wrongColor: 'bg-teal-800',
-        rightColor: 'bg-teal-900'
-    },
-    {
-        wrongColor: 'bg-slate-600',
-        rightColor: 'bg-gray-600'
-    },
-    {
-        wrongColor: 'bg-amber-400',
-        rightColor: 'bg-yellow-400'
-    },
-    {
-        wrongColor: 'bg-emerald-200',
-        rightColor: 'bg-green-200'
-    },
-    {
-        wrongColor: 'bg-indigo-700',
-        rightColor: 'bg-blue-800'
-    },
-    {
-        wrongColor: 'bg-teal-800',
-        rightColor: 'bg-teal-900'
-    },
-    {
-        wrongColor: 'bg-slate-600',
-        rightColor: 'bg-gray-600'
-    },
-    {
-        wrongColor: 'bg-amber-400',
-        rightColor: 'bg-yellow-400'
-    },
-    {
-        wrongColor: 'bg-emerald-200',
-        rightColor: 'bg-green-200'
-    },
-    {
-        wrongColor: 'bg-indigo-700',
-        rightColor: 'bg-blue-800'
-    },
-    {
-        wrongColor: 'bg-teal-800',
-        rightColor: 'bg-teal-900'
-    },
-    {
-        wrongColor: 'bg-slate-600',
-        rightColor: 'bg-gray-600'
-    },
-    {
-        wrongColor: 'bg-amber-400',
-        rightColor: 'bg-yellow-400'
-    }
-]
-
-let score = ref(0);
-let start = ref(false);
-let countdown = ref(60);
-let gameEnd = ref(false);
-let username = ref('');
-let input = ref(false)
-let colorSelected = [];
+const colors = ref(colorJson);
+const name = ref('');
+const mainMenu = ref(false);
+const score = ref(0);
+const counter = ref(60);
+let selectedColor = [];
 let timer;
 
+const getName = () => {
+    if (name.value === '' || name.value.length === 0) {
+        return 'Guest';
+    }
+
+    return name.value;
+}
+
 const startGame = () => {
-    if (timer) clearInterval(timer);
+    if (timer) {
+        clearInterval(timer);
+    }
 
-    start.value = true;
-    input.value = true;
-    gameEnd.value = false;
+    mainMenu.value = true;
     score.value = 0;
-    countdown.value = 60;
-    colorSelected = getRandomColor();
+    counter.value = 60;
+    getRandomColors();
+
     timer = setInterval(() => {
-        if (countdown.value === 0) {
-            gameEnd.value = true;
-            start.value = false;
+        if (counter.value === 0) {
             clearInterval(timer);
-            return;
+            mainMenu.value = false;
         }
-        countdown.value--;
-    }, 1000)
+        counter.value--;
+    }, 1000);
 }
 
-const getRandomColor = () => {
-    colorSelected = [];
-    let shuffledColor = randomColor();
-    shuffledColor.sort(() => Math.random() - 0.5);
 
-    return shuffledColor;
-}
-
-const randomColor = () => {
-    let columnNumer = 4;
+const getNumberOfCircles = () => {
+    let circleNumer = 4;
     if (score.value >= 5 && score.value <= 14) {
-        columnNumer = 9;
+        return circleNumer = 9;
+    } else if (score.value >= 15 && score.value <= 24) {
+        return circleNumer = 16;
+    } else if (score.value >= 25) {
+        return circleNumer = 25;
+    } else {
+        return circleNumer;
     }
-    else if (score.value >= 15 && score.value <= 24) {
-        columnNumer = 16;
-    }
-    else if (score.value >= 25) {
-        columnNumer = 25;
-    }
-
-    let color = colorData[Math.floor(Math.random() * colorData.length)];
-    for (let i = 0; i < columnNumer; i++) {
-        if (colorSelected.length < columnNumer - 1) {
-            colorSelected.push(color.wrongColor);
-        }
-        else {
-            colorSelected.push(color.rightColor);
-            break;
-        }
-    }
-    return colorSelected;
 }
 
-const nextLevel = () => {
+const getNumberOfgridColumns = () => {
     if (score.value >= 5 && score.value <= 14) {
         return 'grid-cols-3';
-    }
-    else if (score.value >= 15 && score.value <= 24) {
+    } else if (score.value >= 15 && score.value <= 24) {
         return 'grid-cols-4';
-    }
-    else if (score.value >= 25) {
+    } else if (score.value >= 25) {
         return 'grid-cols-5';
-    }
-    else {
+    } else {
         return 'grid-cols-2';
     }
 }
 
-const checkAnswer = (color) => {
-    let oddColor = colorSelected.filter((color) => {
-        return colorSelected.indexOf(color) === colorSelected.lastIndexOf(color);
-    })
-
-    if (color === oddColor[0]) {
-        score.value++;
-        colorSelected = getRandomColor();
+const getSizeOfCircles = () => {
+    if (score.value >= 5 && score.value <= 14) {
+        return 'w-24 h-24';
+    } else if (score.value >= 15 && score.value <= 24) {
+        return 'w-20 h-20';
+    } else if (score.value >= 25) {
+        return 'w-16 h-16';
+    } else {
+        return 'w-32 h-32';
     }
 }
 
-const getUsername = () => {
-    if (username.value === '' || username.value === null || username.value.length === 0) {
-        return username.value = 'Guest';
+const getRandomColors = () => {
+    selectedColor = [];
+    const random = Math.floor(Math.random() * colors.value.length);
+    const color = colors.value[random];
+    for (let i = 0; i < getNumberOfCircles(); i++) {
+        if (i < getNumberOfCircles() - 1) {
+            selectedColor.push(color.primaryColor);
+        } else {
+            selectedColor.push(color.secondaryColor);
+        }
     }
-    return username.value;
+    return selectedColor.sort(() => Math.random() - 0.5)
+}
+
+const checkAnswer = (color) => {
+    let secondaryColor = selectedColor.find((color) => selectedColor.indexOf(color) === selectedColor.lastIndexOf(color));
+
+    if (color === secondaryColor) {
+        score.value++;
+        getRandomColors();
+    }
+}
+
+const setMainMenu = () => {
+    mainMenu.value = !mainMenu.value;
 }
 </script>
  
 <template>
-    <div class="w-screen h-screen dark">
-        <div class="w-full h-full flex">
-            <div class="w-9/12 h-5/6 flex m-auto bg-slate-200 dark:bg-slate-700 shadow-md rounded-2xl">
-                <div class="form-control w-full max-w-xs m-auto" v-if="!input">
-                    <label class="label">
-                        <span class="label-text font-bold">What is your name?</span>
-                    </label>
+    <div class="w-screen h-screen" style="background-color: #2A303C;">
+        <div class="w-full h-full" v-show="!mainMenu">
+            <div class="w-5/12 mx-auto mt-6">
+                <img src="./assets/images/logo.png" alt="Catch The Wrong Color">
+            </div>
 
-                    <input type="text" placeholder="Enter your name" class="input input-bordered"
-                        v-model.trim="username" />
-                    <button class="btn mt-3" @click="startGame()">Start Game</button>
+            <div class="w-1/3 h-2/6 mx-auto mt-6 rounded-3xl shadow-lg" style="background-color: #334155;">
+                <div class="flex flex-col items-center">
+                    <input type="text" placeholder="Type your name..." class="input w-5/12 mt-14" v-model.trim="name" />
+                    <button class="btn w-5/12 h-1/6 mt-2" style="background-color: #111B2E;" @click="startGame()">Let's
+                        Start!</button>
+                    <hr class="w-7/12 mt-9" style="border-color: gray;">
+                    <label for="how-to-play" class="btn w-5/12 mt-8" style="background-color: #111B2E;">How to
+                        play</label>
                 </div>
+            </div>
+        </div>
 
-                <div class="w-full flex flex-col items-center" v-if="input">
-                    <div class="flex flex-row mb-5 mt-10 gap-10">
-                        <p class="text-lg font-bold m-auto">Username: {{ getUsername() }}</p>
-                        <p class="text-lg font-bold m-auto">Score: {{ score }}</p>
-                    </div>
+        <div class="w-full h-full" v-show="mainMenu">
+            <div class="w-full h-1/6">
+                <div class="w-1/12 mt-6 ml-6">
+                    <img src="./assets/images/logo.png" alt="Catch The Wrong Color">
+                </div>
+            </div>
 
-                    <div class="flex mb-10 mt-10 gap-10">
-                        <p class="font-mono text-5xl text-center">{{ countdown }}</p>
-                        <button class="btn" @click="startGame()">restart game</button>
-                    </div>
+            <div class="w-full h-full">
+                <div class="w-full h-full flex flex-col">
+                    <div class="w-full h-full flex flex-row">
+                        <div class="w-1/4 h-1/3 flex flex-col">
+                            <p class="text-lg ml-80">
+                                <span class="font-bold">Player: </span>
+                                {{ getName() }}
+                            </p>
+                            <p class="text-lg ml-80">
+                                <span class="font-bold">Score: </span>
+                                {{ score }}
+                            </p>
+                        </div>
 
-                    <div class="flex flex-col text-center mt-32" v-show="gameEnd">
-                        <p class="text-2xl font-bold">Congrats {{ getUsername() }}, the game is over!</p>
-                        <p class="text-2xl font-bold">your score is {{ score }}</p>
-                    </div>
+                        <div class="w-2/4 h-full">
+                            <div class="w-3/6 h-full rounded-3xl shadow-lg mx-auto" style="background-color: #334155;">
+                                <div class="w-full h-full flex flex-col">
+                                    <p class="text-center text-5xl font-mono mt-7">{{ counter }}</p>
 
-                    <div class="grid gap-3" :class="nextLevel()" v-show="start">
-                        <div v-for="(color, index) in colorSelected" :key="index" class="w-24 h-24 rounded-full"
-                            :class="color" @click="checkAnswer(color)">
+                                    <div class="grid gap-1 m-auto mt-10" :class="getNumberOfgridColumns()">
+                                        <div class="rounded-full" v-for="(color, index) in selectedColor"
+                                            :key="index" :class="`${color} ${getSizeOfCircles()}`"
+                                            @click="checkAnswer(color)"></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
+                    <div class="w-full h-full flex flex-row justify-center gap-5">
+                        <button class="btn mt-6" style="background-color: #AC9C48; color: white;" @click="setMainMenu()">Main
+                            menu</button>
+                        <label for="restart-game" class="btn mt-6"
+                            style="background-color: #AC9C48; color: white;">Restart Game</label>
+                    </div>
                 </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- modal how to play -->
+    <input type="checkbox" id="how-to-play" class="modal-toggle" />
+    <div class="modal">
+        <div class="modal-box relative">
+            <label for="how-to-play" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <h3 class="text-lg font-bold">How to play?</h3>
+            <p class="py-4">Find the different color, once you find it, click on it.</p>
+        </div>
+    </div>
+
+    <!-- modal restart game -->
+    <input type="checkbox" id="restart-game" class="modal-toggle" />
+    <div class="modal">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!
+            </p>
+            <div class="modal-action">
+                <label for="restart-game" class="btn">No thanks</label>
+                <label for="restart-game" class="btn btn-error" @click="startGame()">Yay!</label>
             </div>
         </div>
     </div>
 </template>
  
 <style scoped>
+input {
+    color: #000000;
+    background-color: #A1A7B3;
+}
 
+input::placeholder {
+    color: #000000;
+    text-align: center;
+}
+
+input:focus::-webkit-input-placeholder {
+    color: transparent;
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,31 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  // add all color from colors.json to safelist
+  safelist: [
+    'bg-red-700',
+    'bg-red-800',
+    'bg-blue-600',
+    'bg-blue-700',
+    'bg-green-400',
+    'bg-green-500',
+    'bg-yellow-400',
+    'bg-yellow-500',
+    'bg-purple-600',
+    'bg-purple-500',
+    'bg-pink-900',
+    'bg-rose-900',
+    'bg-indigo-800',
+    'bg-indigo-700',
+    'bg-neutral-900',
+    'bg-neutral-800',
+    'bg-emerald-200',
+    'bg-green-200',
+    'bg-blue-800',
+    'bg-teal-800',
+    'bg-teal-900',
+    'bg-slate-600',
+    'bg-gray-600',
+    'bg-amber-400'
+  ]
 }
